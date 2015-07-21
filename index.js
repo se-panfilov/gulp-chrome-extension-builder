f5106fb0eb9ac6a44869e8178cced29414a015c1
S.Panfilov <se-panfilov@ya.ru>
2015-07-21 10:58:05 +0800

    fixes

--- index.js
+++ index.js
@@ -3,35 +3,36 @@
 var fs = require("fs");
 var ChromeExtension = require("crx");
 var join = require("path").join;
-var Stream = require('stream');
-//var Path = require('path');
+var through = require('through');
 
-function gulpCrxPkg(obj) {
+module.exports = function (opts) {
+    opts = opts || {};
 
-    var stream = new Stream.Transform({objectMode: true});
+    var extensionData = {};
+    extensionData.codebase = opts.codebase;
+    privateKey.codebase = fs.readFileSync(join(opts.privateKey.dirname, opts.privateKey.name));
 
-    var crx = new ChromeExtension({
-        codebase: "http://localhost:8000/myFirstExtension.crx",
-        privateKey: fs.readFileSync(join(__dirname, "key.pem"))
-    });
+    var crx = new ChromeExtension(extensionData);
 
-    stream._transform = function (file, unused, callback) {
+    //var crx = new ChromeExtension({
+    //    codebase: "http://localhost:8000/myFirstExtension.crx",
+    //    privateKey: fs.readFileSync(join(__dirname, "key.pem"))
+    //});
 
-        crx.load(join(__dirname, "myFirstExtension"))
+    var extName = opts.extName || __dirname;
+
+    return through(function (file) {
+        console.log('start');
+        crx.load(join(__dirname, extName))
             .then(function () {
                 return crx.pack().then(function (crxBuffer) {
                     var updateXML = crx.generateUpdateXML();
 
                     fs.writeFile(join(__dirname, "update.xml"), updateXML);
-                    fs.writeFile(join(__dirname, "myFirstExtension.crx"), crxBuffer);
+                    fs.writeFile(join(__dirname, extName + ".crx"), crxBuffer);
                 });
             });
-
-        callback(null, file);
-    };
-
-    return stream;
-
-}
-
-module.exports = gulpCrxPkg;
+    }, function () {
+        console.log('end');
+    });
+};
