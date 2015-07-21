fbaa562ee431d69adfd2cc02680831c6f6922424
S.Panfilov <se-panfilov@ya.ru>
2015-07-21 10:43:42 +0800

    fixes

--- index.js
+++ index.js
@@ -1,76 +1,37 @@
 'use strict';
 
-var shell = require('shell');
+var fs = require("fs");
+var ChromeExtension = require("crx");
+var join = require("path").join;
+var Stream = require('stream');
+//var Path = require('path');
 
-function gulpChromeExensionPacker() {
-    var app = new shell({chdir: __dirname});
+function gulpCrxPkg(obj) {
 
-    app.on('quit', function(){
-        //app.client.quit();
+    var stream = new Stream.Transform({objectMode: true});
+
+    var crx = new ChromeExtension({
+        codebase: "http://localhost:8000/myFirstExtension.crx",
+        privateKey: fs.readFileSync(join(__dirname, "key.pem"))
     });
+
+    stream._transform = function (file, unused, callback) {
+
+        crx.load(join(__dirname, "myFirstExtension"))
+            .then(function () {
+                return crx.pack().then(function (crxBuffer) {
+                    var updateXML = crx.generateUpdateXML();
+
+                    fs.writeFile(join(__dirname, "update.xml"), updateXML);
+                    fs.writeFile(join(__dirname, "myFirstExtension.crx"), crxBuffer);
+                });
+            });
+
+        callback(null, file);
+    };
+
+    return stream;
+
 }
 
-
-//var Stream = require('stream');
-//var Path = require('path');
-//
-//function gulpRename(obj) {
-//
-//    var stream = new Stream.Transform({objectMode: true});
-//
-//    function parsePath(path) {
-//        var extname = Path.extname(path);
-//        return {
-//            dirname: Path.dirname(path),
-//            basename: Path.basename(path, extname),
-//            extname: extname
-//        };
-//    }
-//
-//    stream._transform = function (file, unused, callback) {
-//
-//        var parsedPath = parsePath(file.relative);
-//        var path;
-//
-//        var type = typeof obj;
-//
-//        if (type === 'string' && obj !== '') {
-//
-//            path = obj;
-//
-//        } else if (type === 'function') {
-//
-//            obj(parsedPath);
-//            path = Path.join(parsedPath.dirname, parsedPath.basename + parsedPath.extname);
-//
-//        } else if (type === 'object' && obj !== undefined && obj !== null) {
-//
-//            var dirname = 'dirname' in obj ? obj.dirname : parsedPath.dirname,
-//                prefix = obj.prefix || '',
-//                suffix = obj.suffix || '',
-//                basename = 'basename' in obj ? obj.basename : parsedPath.basename,
-//                extname = 'extname' in obj ? obj.extname : parsedPath.extname;
-//
-//            path = Path.join(dirname, prefix + basename + suffix + extname);
-//
-//        } else {
-//
-//            callback(new Error('Unsupported renaming parameter type supplied'), undefined);
-//            return;
-//
-//        }
-//
-//        file.path = Path.join(file.base, path);
-//
-//        // Rename sourcemap if present
-//        if (file.sourceMap) {
-//            file.sourceMap.file = file.relative;
-//        }
-//
-//        callback(null, file);
-//    };
-//
-//    return stream;
-//}
-
-module.exports = gulpChromeExensionPacker;
+module.exports = gulpCrxPkg;
