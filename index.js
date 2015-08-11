14d458c1ada313dafe65f78bb05ff4056f882ccc
S.Panfilov <se-panfilov@ya.ru>
2015-08-11 13:50:43 +0800

    fixes

--- index.js
+++ index.js
@@ -4,103 +4,50 @@ var fs = require("fs");
 var ChromeExtension = require("crx");
 var join = require("path").join;
 var through = require('through2');
+var gutil = require('gulp-util');
+var merge = require('merge');
 
-module.exports = function (opts) {
-    opts = opts || {};
+var PluginError = gutil.PluginError;
+
+
+module.exports = function (opt) {
 
     var REQUIRED_PROPERTIES = ['manifest_version', 'name', 'version'];
 
-    function getExtDir(rootPath) {
-        return join(rootPath, extName);
-    }
+    function transform(file, enc, cb) {
+        //if (file.isNull()) return cb(null, file);
+        if (file.isStream()) return cb(new PluginError('gulp-crx-pkg', 'Streaming not supported'));
 
-    var extensionData = {};
-    extensionData.codebase = opts.codebase;
-    extensionData.zip = extensionData.zip || false;
-    var key = null;
-
-    if (opts.privateKey) {
-        key = {};
-        key.dirname = opts.privateKey.dirname;
-        key.name = opts.privateKey.name;
-        privateKey.privateKey = fs.readFileSync(join(key.dirname, key.name));
-    }
+        var options = merge({
+            some: false
+        }, opt);
 
 
-    //var crx = new ChromeExtension({
-    //    codebase: "http://localhost:8000/myFirstExtension.crx",
-    //    privateKey: fs.readFileSync(join(__dirname, "key.pem"))
-    //});
+        var extensionData = {};
+        extensionData.codebase = options.codebase;
+        extensionData.zip = extensionData.zip || false;
+        var key = null;
 
-    var extName = opts.extName || 'demoExt';
-
-    function load(crx, file) {
-        return crx.load(getExtDir(file.path)).then(function () {
-            return crx.loadContents();
-        }, function (e) {
-            console('Error!!!');
-            console.log(e);
-        });
-    }
-
-    function makeZip(crx, promise, path, extName) {
-        console.log('zip');
-        return promise.then(function (archiveBuffer) {
-            console.log('zip run');
-            var updateXML = crx.generateUpdateXML();
-            fs.writeFile(join(path, "update.xml"), updateXML);
-            fs.writeFile(join(path, extName + '.zip'), archiveBuffer);
-            return crx.pack(archiveBuffer);
-        }, function (e) {
-            console('Error!!!');
-            console.log(e);
-        })
-    }
-
-    function mareCrx(crx, promise, path, extName) {
-        console.log('crx');
-        return promise.then(function (crxBuffer) {
-            console.log('crx run');
-            var updateXML = crx.generateUpdateXML();
-            fs.writeFile(join(path, "update.xml"), updateXML);
-            fs.writeFile(join(path, extName + '.crx'), crxBuffer);
-        }, function (e) {
-            console('Error!!!');
-            console.log(e);
-        });
-    }
-
-    return through.obj(function (file, enc, cb) {
-        if (file.isNull()) {
-            cb(null, file);
-            return;
+        if (options.privateKey) {
+            key = {};
+            key.dirname = options.privateKey.dirname;
+            key.name = options.privateKey.name;
+            privateKey.privateKey = fs.readFileSync(join(key.dirname, key.name));
         }
 
-        this.queue(file);
-
-        console.log('start');
-
         var crx = new ChromeExtension(extensionData);
-        var crxPromise = load(crx, file);
-        ///if (extensionData.zip) {
-        makeZip(crx, crxPromise, file.path, extName);
-        //} else {
-        mareCrx(crx, crxPromise, file.path, extName);
-        //}
+        console.log('crx');
 
-        //crx.load(getExtDir(file.path)).then(function () {
-        //    return crx.pack().then(function (crxBuffer) {
-        //        var updateXML = crx.generateUpdateXML();
-        //
-        //        fs.writeFile(join(__dirname, "update.xml"), updateXML);
-        //        fs.writeFile(join(__dirname, extName + ".crx"), crxBuffer);
-        //    });
-        //});
 
-        console.log('done');
+        crx.load(file.path).then(function () {
+            return crx.loadContents();
+        }).then(function (archiveBuffer) {
+            fs.writeFile(file.path + '.zip', archiveBuffer);
+            return crx.pack(archiveBuffer);
+        }).then(function (crxBuffer) {
+            fs.writeFile(file.path + '.crx', crxBuffer);
+        });
+    }
 
-        cb(null, file);
-    }, function () {
-        console.log('end');
-    });
+    return through.obj(transform);
 };
