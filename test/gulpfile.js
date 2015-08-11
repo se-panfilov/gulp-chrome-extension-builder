941d27dc0a5cbd4e16d4ad9eee03c0c75d055432
S.Panfilov <se-panfilov@ya.ru>
2015-08-11 15:53:40 +0800

    fixes

--- test/gulpfile.js
+++ test/gulpfile.js
@@ -6,7 +6,21 @@ gulp.task('gcp', function () {
         .pipe(gcp())
         .pipe(gulp.dest('./'))
 });
+gulp.task('gcp-zip', function () {
+    return gulp.src('./test_ext')
+        .pipe(gcp({
+            zip: true,
+            crx: false
+        }))
+        .pipe(gulp.dest('./'))
+});
 
 gulp.task('default', function () {
     gulp.start('gcp');
 });
+
+gulp.task('zip', function () {
+    gulp.start('gcp-zip');
+});
+
+
